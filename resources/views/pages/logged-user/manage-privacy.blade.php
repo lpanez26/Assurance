10f9f98d1c476bbc90b253e3a22a3f349a69b844
unknown <miroslav.nedelchev@dentacoin.com>
2019-01-22 17:18:50 +0200

    test my profile inner pages

--- resources/views/pages/logged-user/manage-privacy.blade.php
+++ resources/views/pages/logged-user/manage-privacy.blade.php
@@ -3,7 +3,7 @@
     <section class="manage-privacy padding-top-100">
         <div class="container">
             <div class="row">
-                <div class="col-xs-12">
+                <div class="col-xs-12 fs-0">
                     @include('pages.logged-user.my-profile-menu')
                     <div class="my-profile-page-content inline-block-top">
                         MANAGE PRIVACY
--- resources/views/pages/logged-user/my-profile-menu.blade.php
+++ resources/views/pages/logged-user/my-profile-menu.blade.php
@@ -8,9 +8,38 @@
         </div>
         <nav>
             <ul itemscope="" itemtype="http://schema.org/SiteNavigationElement">
-                <li><a href="{{ route('my-profile') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'my-profile') class="active" @endif itemprop="url"><span itemprop="name">My Wallet</span></a></li>
-                <li><a href="{{ route('edit-account') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'edit-account') class="active" @endif itemprop="url"><span itemprop="name">Edit Account</span></a></li>
-                <li><a href="{{ route('manage-privacy') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'manage-privacy') class="active" @endif itemprop="url"><span itemprop="name">Manage Privacy</span></a></li>
+                <li>
+                    <a href="{{ route('my-profile') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'my-profile') class="active" @endif itemprop="url">
+                        <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
+                            <img alt="" src="/assets/uploads/contracts-list.svg"/>
+                        </figure>
+                        <span itemprop="name">My Wallet</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('edit-account') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'edit-account') class="active" @endif itemprop="url">
+                        <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
+                            <img alt="" src="/assets/uploads/contracts-list.svg"/>
+                        </figure>
+                        <span itemprop="name">Edit Account</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('manage-privacy') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'manage-privacy') class="active" @endif itemprop="url">
+                        <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
+                            <img alt="" src="/assets/uploads/contracts-list.svg"/>
+                        </figure>
+                        <span itemprop="name">Manage Privacy</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('my-contracts') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'my-contracts') class="active" @endif itemprop="url">
+                        <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
+                            <img alt="Contracts list" src="/assets/uploads/contracts-list.svg"/>
+                        </figure>
+                        <span itemprop="name">My contracts</span>
+                    </a>
+                </li>
                 <li><a href="{{ route('user-logout') }}" itemprop="url"><span itemprop="name">Log out</span></a></li>
             </ul>
         </nav>
