e3c3cec72d9d11f6b12653f211874f5d752398bf
unknown <miroslav.nedelchev@dentacoin.com>
2019-01-22 12:45:49 +0200

    test patient login

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -28,7 +28,8 @@ class PatientController extends Controller {
     }
 
     public function getPatientAccess()    {
-        var_dump(session('have_contracts'));
+        /*var_dump(session('have_contracts'));
+        die();*/
         /*session(['logged_user' => [
             'token' => 'test-token',
             'email' => 'test@abv.bg',
