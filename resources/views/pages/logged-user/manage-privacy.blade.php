a02835484fb9c5e6950906648277181d403ee97a
unknown <miroslav.nedelchev@dentacoin.com>
2019-01-30 16:06:57 +0200

    test my profile look

--- resources/views/pages/logged-user/manage-privacy.blade.php
+++ resources/views/pages/logged-user/manage-privacy.blade.php
@@ -6,7 +6,12 @@
                 <div class="col-xs-12 fs-0">
                     @include('pages.logged-user.my-profile-menu')
                     <div class="my-profile-page-content inline-block-top">
-                        MANAGE PRIVACY
+                        <div class="profile-page-title">
+                            <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
+                                <img alt="Privacy icon" src="/assets/uploads/privacy-icon.svg"/>
+                            </figure>
+                            <h2 class="fs-26 lato-semibold inline-block">Manage privacy</h2>
+                        </div>
                     </div>
                 </div>
             </div>
