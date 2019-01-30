fe03667da49af7ac9870220694b7671b3510cb13
unknown <miroslav.nedelchev@dentacoin.com>
2019-01-30 10:04:17 +0200

    pushing images

--- resources/views/pages/logged-user/my-profile-menu.blade.php
+++ resources/views/pages/logged-user/my-profile-menu.blade.php
@@ -11,7 +11,7 @@
                 <li>
                     <a href="{{ route('my-profile') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'my-profile') class="active" @endif itemprop="url">
                         <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
-                            <img alt="" src="/assets/uploads/contracts-list.svg"/>
+                            <img alt="" src="/assets/uploads/wallet-icon.svg"/>
                         </figure>
                         <span itemprop="name">My Wallet</span>
                     </a>
@@ -19,7 +19,7 @@
                 <li>
                     <a href="{{ route('edit-account') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'edit-account') class="active" @endif itemprop="url">
                         <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
-                            <img alt="" src="/assets/uploads/contracts-list.svg"/>
+                            <img alt="" src="/assets/uploads/edit-account-icon.svg"/>
                         </figure>
                         <span itemprop="name">Edit Account</span>
                     </a>
@@ -27,7 +27,7 @@
                 <li>
                     <a href="{{ route('manage-privacy') }}" @if(!empty(Route::current()) && Route::current()->getName() == 'manage-privacy') class="active" @endif itemprop="url">
                         <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
-                            <img alt="" src="/assets/uploads/contracts-list.svg"/>
+                            <img alt="" src="/assets/uploads/privacy-icon.svg"/>
                         </figure>
                         <span itemprop="name">Manage Privacy</span>
                     </a>
@@ -40,7 +40,14 @@
                         <span itemprop="name">My contracts</span>
                     </a>
                 </li>
-                <li><a href="{{ route('user-logout') }}" itemprop="url"><span itemprop="name">Log out</span></a></li>
+                <li>
+                    <a href="{{ route('user-logout') }}" itemprop="url">
+                        <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
+                            <img alt="Contracts list" src="/assets/uploads/logout.svg"/>
+                        </figure>
+                        <span itemprop="name">Log out</span>
+                    </a>
+                </li>
             </ul>
         </nav>
     </div>
