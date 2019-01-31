b317c765708b8818dae977106d31e5f97a16d496
unknown <miroslav.nedelchev@dentacoin.com>
2019-01-31 15:06:51 +0200

    test manage-privacy design

--- resources/views/pages/logged-user/manage-privacy.blade.php
+++ resources/views/pages/logged-user/manage-privacy.blade.php
@@ -14,7 +14,7 @@
                         </div>
                         <div class="delete padding-top-60 padding-bottom-50">
                             <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block-top">
-                                <img alt="" src="/assets/uploads/avatar-icon.svg"/>
+                                <img alt="" src="/assets/images/avatar-icon.svg"/>
                             </figure>
                             <div class="text inline-block-top">
                                 <h3 class="fs-20 padding-bottom-20 lato-semibold dark-color">Delete My Profile</h3>
@@ -26,7 +26,7 @@
                         </div>
                         <div class="download padding-top-60 padding-bottom-50">
                             <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block-top">
-                                <img alt="" src="/assets/uploads/avatar-icon.svg"/>
+                                <img alt="" src="/assets/images/avatar-icon.svg"/>
                             </figure>
                             <div class="text inline-block-top">
                                 <h3 class="fs-20 padding-bottom-20 lato-semibold dark-color">Download My Personal Data</h3>
