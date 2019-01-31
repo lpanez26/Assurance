9088545025ca97ca5611cb0997151280e7b15659
unknown <miroslav.nedelchev@dentacoin.com>
2019-01-31 11:40:33 +0200

    dentist login

--- app/Http/Controllers/DentistController.php
+++ app/Http/Controllers/DentistController.php
@@ -116,6 +116,8 @@ class DentistController extends Controller
 
         //handle the API response
         $api_response = (new APIRequestsController())->dentistLogin($data);
+        var_dump($api_response);
+        die();
         if($api_response->success) {
             $session_arr = [
                 'token' => $api_response->token,
