2b784ca5cd46650fd32cafdd6b622ad7815bcbb6
unknown <miroslav.nedelchev@dentacoin.com>
2019-01-31 15:12:57 +0200

    test manage-privacy design

--- resources/views/pages/logged-user/manage-privacy.blade.php
+++ resources/views/pages/logged-user/manage-privacy.blade.php
@@ -10,14 +10,14 @@
                             <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block">
                                 <img alt="Privacy icon" src="/assets/uploads/privacy-icon.svg"/>
                             </figure>
-                            <h2 class="fs-24 lato-semibold inline-block">Manage privacy</h2>
+                            <h2 class="fs-24 lato-bold inline-block">Manage privacy</h2>
                         </div>
                         <div class="delete padding-top-60 padding-bottom-50 fs-0">
                             <figure itemscope="" itemtype="http://schema.org/ImageObject" class="inline-block-top">
                                 <img alt="Cancel icon" src="/assets/uploads/cancel.svg"/>
                             </figure>
                             <div class="text inline-block-top">
-                                <h3 class="fs-20 padding-bottom-20 lato-semibold dark-color">Delete My Profile</h3>
+                                <h3 class="fs-20 padding-bottom-20 lato-bold dark-color">Delete My Profile</h3>
                                 <div class="fs-16 dark-color">We can delete your Profile along with all your personal data from our servers. Just keep in mind that this will terminate your account irreversibly. If you are sure about that, just click the button below.</div>
                             </div>
                             <div class="btn-container text-right padding-top-30">
@@ -29,7 +29,7 @@
                                 <img alt="Download icon" src="/assets/uploads/download.svg"/>
                             </figure>
                             <div class="text inline-block-top">
-                                <h3 class="fs-20 padding-bottom-20 lato-semibold dark-color">Download My Personal Data</h3>
+                                <h3 class="fs-20 padding-bottom-20 lato-bold dark-color">Download My Personal Data</h3>
                                 <div class="fs-16 dark-color">You can request to receive all your personal data stored on our servers. Just click on the button below to download it.</div>
                             </div>
                             <div class="btn-container text-right padding-top-30">
